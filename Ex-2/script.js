43e00c5ff79726a93ba55682045f6f995042405d
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-16 00:48:51 +0200

    Spostato Script all'inizio della pagina, per far partire il codice prima del caricamento compkleto della pagina

---
+++ Ex-2/script.js
@@ -0,0 +1,7 @@
+// Il software deve chiedere all’utente il suo nome e il sesso con due prompt diversi.
+// Sulla pagina html deve apparire “Ciao <nome>“,
+// il colore del nome deve essere azzurro o rosa a seconda del sesso inserito;
+
+
+var nome = prompt('Inserisci qui il tuo nome');
+var sesso = prompt('Inserisci qui il tuo sesso');
